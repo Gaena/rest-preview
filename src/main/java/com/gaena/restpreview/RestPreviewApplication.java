8e9f7433df0e073cea75468a37c26b77867c81dd
gaena <ilyaz214@gmail.com>
2019-05-28 00:45:13 +0400

    Added test controller , Initial

---
+++ src/main/java/com/gaena/restpreview/RestPreviewApplication.java
@@ -0,0 +1,13 @@
+package com.gaena.restpreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestPreviewApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/gaena/restpreview/controllers/PreviewController.java
@@ -0,0 +1,13 @@
+package com.gaena.restpreview.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PreviewController {
+
+
+
+    public void test(){
+        System.out.println("Init");
+    }
+}
